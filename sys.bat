54623338bd08007864c906d681f1a39efe35c646
riatorz <riatorz658@gmail.com>
2021-11-25 09:32:02 +0300

    add pillow

--- sys.bat
+++ sys.bat
@@ -1,6 +1,6 @@
 @echo off
 
 pip install qrcode
-echo %UserProfile%\AppData\Local\Programs\Python\Python37\Lib\site-packages;%UserProfile%\AppData\Local\Programs\Python\Python37
-echo Ustteki dizini kopyalayin. Dokumanda belirtilen dosyadaki bilgilere gore yapistirin.
+pip install pillow
+
 pause
