74f4efc835937e9c2fde1ac1aac155a22f5d5db9
riatorz <riatorz658@gmail.com>
2021-11-25 09:30:42 +0300

    bug fix

--- NXQrCode.py
+++ NXQrCode.py
@@ -24,7 +24,7 @@ NxType = 0
 NxSubtype = 0
 #Function Tabular Notes
 while True:
-    tmpTabNote = theUfSession.Obj.CycleObjsInPart(workPart.Tag, NXOpen.UF.UFConstants.UF_tabular_note_type, tmpTabNote)# değiştiiiii
+    tmpTabNote = theUfSession.Obj.CycleObjsInPart(workPart.Tag, NXOpen.UF.UFConstants.UF_tabular_note_type, tmpTabNote)
     if tmpTabNote != 0:
         NxType , NxSubtype = theUfSession.Obj.AskTypeAndSubtype(tmpTabNote)
         if NxSubtype == NXOpen.UF.UFConstants.UF_tabular_note_subtype:
@@ -39,44 +39,55 @@ while True:
         break
 lw.WriteLine(str(f"Number of tabular notes found: {len(myTabNotes)}\n"))
 ###############################
-#lw.WriteLine(str(f"Number of tabular notes found: {len(myTabNotes)}\n"))
-#lw.WriteLine(str(f'First tabular note info:\n'))
-numSections = theUfSession.Tabnot.AskNmSections(myTabNotes[0])
-#lw.WriteLine(str(f'Number of sections in tabular note: {numSections}'))
-numRows = theUfSession.Tabnot.AskNmRows(myTabNotes[0])
-#lw.WriteLine(str(f'Number of rows in tabular note: {numRows}'))
-numCols = theUfSession.Tabnot.AskNmColumns(myTabNotes[0])
-#lw.WriteLine(str(f'Number of columns in tabular note: {numCols}'))
+trueTable = 0
+trueRows = 6
+trueCols = 8
+for i in range(len(myTabNotes)):
+    lw.WriteLine(str(f'{i+1}. tabular note info:\n'))
+    numSections = theUfSession.Tabnot.AskNmSections(myTabNotes[i])
+    lw.WriteLine(str(f'Number of sections in tabular note: {numSections}'))
+    numRows = theUfSession.Tabnot.AskNmRows(myTabNotes[i])
+    lw.WriteLine(str(f'Number of rows in tabular note: {numRows}'))
+    numCols = theUfSession.Tabnot.AskNmColumns(myTabNotes[i])
+    lw.WriteLine(str(f'Number of columns in tabular note: {numCols}'))
+    if numRows==trueRows and numCols==trueCols:
+        trueTable = i 
+#if you had a one tabular note please use
+# numSections = theUfSession.Tabnot.AskNmSections(myTabNotes[i])
+# lw.WriteLine(str(f'Number of sections in tabular note: {numSections}'))
+# numRows = theUfSession.Tabnot.AskNmRows(myTabNotes[i])
+# lw.WriteLine(str(f'Number of rows in tabular note: {numRows}'))
+# numCols = theUfSession.Tabnot.AskNmColumns(myTabNotes[i])
+# lw.WriteLine(str(f'Number of columns in tabular note: {numCols}'))
+
 #coltags and celltag
-myTags = ["41","20","40","24","27","06"]
+myTags = ["06","20","22","23","24","25","26","27","40","41","45"]
 tagDict = {}
 qrTextDict = {}
 qrText = ""
-for j in range(0, numRows):
-        rowTag = theUfSession.Tabnot.AskNthRow(myTabNotes[0], j)
-        for k in range(0, numCols):
-            colTag =theUfSession.Tabnot.AskNthColumn(myTabNotes[0], k)
+for j in range(0, trueRows):
+        rowTag = theUfSession.Tabnot.AskNthRow(myTabNotes[trueTable], j)
+        for k in range(0, trueCols):
+            colTag =theUfSession.Tabnot.AskNthColumn(myTabNotes[trueTable], k)
             cellTag = theUfSession.Tabnot.AskCellAtRowCol(rowTag, colTag)
             cellText = ""
             evalCellText = ""
             cellText = theUfSession.Tabnot.AskCellText(cellTag)
             evalCellText = theUfSession.Tabnot.AskEvaluatedCellText(cellTag)
-            lw.WriteLine(f"{colTag}{cellText}")
+            #lw.WriteLine(f"{colTag}-- [{j}{k}] {cellText}")
             if cellText != "" and f"{j}{k}" in myTags:
                 tagDict.update({f"{j}{k}":cellTag})
-                # lw.WriteLine(str(f'Cell[{j},{k}]'))
-                # lw.WriteLine(str(f'text: {cellText}'))
-                # lw.WriteLine(str(f'evaluated text: {evalCellText}'))
+                lw.WriteLine(str(f'Cell[{j},{k}] = {cellText}'))
 for i in tagDict:
     AcellText = theUfSession.Tabnot.AskCellText(tagDict[i])
     qrTextDict.update({f"{tagDict[i]}":AcellText})
-
+lw.WriteLine(f"{qrTextDict}")
 for i in qrTextDict:
     lw.WriteLine(f"{qrTextDict[i]}")
     qrText += f"{qrTextDict[i]};"
 qrText = qrText[:-1]
 qrpath = ''.join(str(i)+"\\" for i in workPart.FullPath.split("\\")[:3])+imgpath
-img=qrcode.make(f"{qrText}")#DrawingCodeNumber-Drawing-Date-Mat-PartNumber-HeatTreatment(ExtraProcess)
+img=qrcode.make(f"{qrText}")
 img.save(qrpath)
 lw.WriteLine(f"qrCode belirtilen dizine kaydedildi.")
 lw.WriteLine(f"Dizin: {qrpath}\nDosya ismi: {imgpath}")
